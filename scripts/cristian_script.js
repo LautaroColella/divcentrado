721cca047fa8f8e6105a5ff3a46a77a94f7307ab
Humberto Cristian Paez <crishpaez1993@gmail.com>
2025-09-17 20:21:22 -0300

    Tarjeta presentacion cristian, html, css, y js

---
+++ scripts/cristian_script.js
@@ -0,0 +1,28 @@
+// --- menu hamburguesa ---
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+// --- card giratoria ---
+document.querySelectorAll(".flip-card").forEach(card => {
+  card.addEventListener("click", () => {
+    card.classList.toggle("flipped");
+  });
+});
+
+
+const frases = [
+  "Lo importante es aprender cosas nuevas",
+  "Trabajar en equipo siempre suma"  
+];
+
+let index = 0;
+function cambiarFrase() {
+  document.getElementById("frase").textContent = frases[index];
+  index = (index + 1) % frases.length;
+}
+setInterval(cambiarFrase, 3000);
+cambiarFrase();
